69a5c0103d7a689c3f927c6d2c3eabb11e269c8a
Kunth Shah <58458110+kunthshah21@users.noreply.github.com>
2023-12-25 23:31:44 +0530

    Initial Commit

---
+++ WordScrable/WordScrableApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrableApp.swift
+//  WordScrable
+//
+//  Created by Kunth Shah on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
